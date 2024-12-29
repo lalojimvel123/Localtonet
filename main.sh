86b6569b6c3514918a5823d07b77ca5da628caea
lalojimvel123 <119811365+lalojimvel123@users.noreply.github.com>
2024-12-29 12:24:37 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,24 +5,14 @@ echo_formatted() {
   echo -e "\033[1;32m$1\033[0m"
 }
 
-# Función para descargar y ejecutar scripts
-download_and_run() {
-  local script_name="$1"
-  local script_url="URL_BASE/${script_name}"  # Ajusta 'URL_BASE' a tu repositorio de GitHub o URL donde se encuentran los scripts
-
-  echo_formatted "Descargando y ejecutando $script_name..."
-  
-  if ! wget -q "$script_url" -O "/tmp/$script_name"; then
-    echo_formatted "Error al descargar $script_name. Verifica la URL y tu conexión a Internet."
-    return 1
-  fi
-
-  chmod +x "/tmp/$script_name"
-  if ! "/tmp/$script_name"; then
-    echo_formatted "Error al ejecutar $script_name."
-  fi
-  
-  rm -f "/tmp/$script_name"  # Limpiar el archivo descargado después de su uso
+# Función para limpiar el directorio actual
+clean_up() {
+  local current_dir=$(pwd)
+  local parent_dir=$(dirname "$current_dir")
+  echo_formatted "Limpiando el directorio $current_dir..."
+  cd "$parent_dir" || exit 1
+  rm -rf "$current_dir"
+  echo_formatted "Directorio eliminado."
 }
 
 # Función principal para el menú
@@ -30,25 +20,26 @@ main_menu() {
   while true; do
     echo_formatted "\nMenú de ejecución:"
     PS3="Elige tu opción: "
-    opciones=("Instalar Localto" "Configurar puertos" "Configurar Token" "Desinstalar Localto" "Salir")
+    opciones=("Instalar Localto" "Configurar puertos" "Configurar Token" "Desinstalar Localto" "Salir y limpiar")
     select opt in "${opciones[@]}"
     do
       case $opt in
         "Instalar Localto")
-          download_and_run "Install_localto.sh"
+          bash "Install_localto.sh"
           ;;
         "Configurar puertos")
-          download_and_run "Port_config.sh"
+          bash "Port_config.sh"
           ;;
         "Configurar Token")
-          download_and_run "Token_config.sh"
+          bash "Token_config.sh"
           ;;
         "Desinstalar Localto")
-          download_and_run "Uninstall_Localto.sh"
+          bash "Uninstall_Localto.sh"
           ;;
-        "Salir")
-          echo_formatted "Saliendo del menú."
-          return
+        "Salir y limpiar")
+          echo_formatted "Saliendo del menú y limpiando..."
+          clean_up
+          exit 0
           ;;
         *) echo_formatted "Opción no válida."
           ;;
