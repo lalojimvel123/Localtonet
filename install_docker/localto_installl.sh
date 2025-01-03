4d4c856481b10444149bf138527a8605ce2071d8
lalojimvel123 <119811365+lalojimvel123@users.noreply.github.com>
2025-01-02 19:27:09 -0600

    Update and rename localto_autoinstalll.sh to localto_installl.sh

---
+++ install_docker/localto_installl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Actualización del sistema (Alpine usa apk en lugar de apt)
+apk update && apk upgrade
+
+# Instala wget y unzip para manejar el descargado y descompresion
+apk add --no-cache wget unzip
+
+# Descargar zip de localto
+wget -O localtonet-linux-arm64.zip "https://localtonet.com/download/localtonet-linux-arm64.zip"
+
+# Decomprime zip de localto
+unzip localtonet-linux-arm64.zip
+
+# Borra zip de localto
+rm -f localtonet-linux-arm64.zip
+
+# Dar permisos de ejecución al binario de Localtonet 
+chmod +x localtonet
+
+# Ejecuta aplicacion - Aquí es donde necesitas automatizar el token
+echo "TU_TOKEN_AQUI" | ./localtonet
